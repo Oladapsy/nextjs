1e268d8a05f9b798d11219787d11d373411bedcc
Oladapsy <dapoodedeyi@yahoo.com>
2025-03-20 07:10:56 -0700

    handling error

--- youtube_nextjs/youtube-nextjs-app/app/(root)/about/page.tsx
+++ youtube_nextjs/youtube-nextjs-app/app/(root)/about/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const About = () => {
-    console.log("I am a server component for about page");
+    // console.log("I am a server component for about page");
+
+    throw new Error("NOT IMPLEMENTED");
     return (
         <>
         <h1>this is the about page</h1>
