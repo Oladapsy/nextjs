2b7d2287d800b7641a9ed5a3b80eadac119598cd
Oladapsy <dapoodedeyi@yahoo.com>
2025-03-20 03:26:58 -0700

    try to see what the component created will be, client / server

--- youtube_nextjs/youtube-nextjs-app/app/page.tsx
+++ youtube_nextjs/youtube-nextjs-app/app/page.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
+  console.log("What am i doing here? -- SERVER/CLINENT?");
   return (
     <h1 className="text-5xl">Welcome to the Youtube Nextjs</h1>
   );
